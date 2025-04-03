19fead5809279b139b1c5c925ed0399ac9ebd3ae
NurettinKoca <udzos@student.kit.edu>
2025-04-03 15:55:17 +0200

    change something about router

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { createApp } = Vue;
-const { createRouter, createWebHashHistory } = VueRouter;
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import { createRouter, createWebHashHistory } from "https://unpkg.com/vue-router@4/dist/vue-router.esm-browser.js";
 
 // Import components
 import Home from './home.js';
